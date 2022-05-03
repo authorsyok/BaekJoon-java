ccc2ec3923c3ff46f302758490e61c0c794f3377
authorsyok <authorsyok@gmail.com>
2022-05-03 16:46:53 +0900

    .

---
+++ src/입출력과사칙연산/_1000.java
@@ -0,0 +1,21 @@
+package 입출력과사칙연산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1000 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a + b);
+	}
+}
