e018becf28bf971ef40e31311acc2851394573d1
authorsyok <authorsyok@gmail.com>
2022-05-04 14:54:22 +0900

    .

---
+++ src/반복문/_15552.java
@@ -0,0 +1,26 @@
+package ¹Ýº¹¹®;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _15552 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < t; i++) {		
+			st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
