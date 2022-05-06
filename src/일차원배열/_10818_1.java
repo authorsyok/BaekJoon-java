80e512c733a5663418d7ee2d252dad992314c0d6
authorsyok <authorsyok@gmail.com>
2022-05-06 16:25:19 +0900

    배열o

---
+++ src/일차원배열/_10818_1.java
@@ -0,0 +1,28 @@
+package 일차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _10818_1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[0] + " " + arr[n-1]);		
+	}
+}
