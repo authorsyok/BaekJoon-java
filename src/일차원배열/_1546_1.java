51f95bf21a354555f2c812bc47086f875de720fd
authorsyok <authorsyok@gmail.com>
2022-05-09 15:46:05 +0900

    배열o

---
+++ src/일차원배열/_1546_1.java
@@ -0,0 +1,33 @@
+package 일차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _1546_1 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		double[] arr = new double[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[n - 1]) * 100);
+		}
+		
+		System.out.println(sum / arr.length);	
+	}
+}
