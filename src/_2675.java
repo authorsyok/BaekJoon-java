6b2b09d9b77674639e4e64956e76a264d7b985bb
authorsyok <authorsyok@gmail.com>
2022-05-12 19:45:27 +0900

    .

---
+++ src/_2675.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2675 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int r = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			
+			for(int j = 0; j < s.length(); j++) {
+				for(int k = 0; k < r; k++) {
+					System.out.print(s.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/문자열/_11654.java
@@ -0,0 +1,13 @@
+package ¹®ÀÚ¿­;
+
+import java.io.IOException;
+
+public class _11654 {
+
+	public static void main(String[] args) throws IOException {
+
+		int input = System.in.read();
+		System.out.println(input);
+		
+	}
+}
