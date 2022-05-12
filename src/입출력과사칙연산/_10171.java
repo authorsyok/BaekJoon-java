cbca22953a3c7ebccc6b6b97fed9480e8e1119d8
authorsyok <authorsyok@gmail.com>
2022-05-12 15:20:01 +0900

    .

---
+++ src/입출력과사칙연산/_10171.java
@@ -0,0 +1,12 @@
+package 입출력과사칙연산;
+
+public class _10171 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
