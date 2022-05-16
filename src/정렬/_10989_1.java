bd707d75525b444da0dc66be65a79932c0656f44
authorsyok <authorsyok@gmail.com>
2022-05-16 22:58:14 +0900

    .

---
+++ src/정렬/_10989_1.java
@@ -0,0 +1,28 @@
+package Á¤·Ä;
+
+import java.io.*;
+import java.util.*;
+
+class _10989_1 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+
+    int[] arr = new int[10001];
+    
+    for(int i = 0; i < n; i++) {
+      arr[Integer.parseInt(br.readLine())]++;
+    }
+
+    for(int i = 1; i < 10001; i++) {
+      while(arr[i] > 0) {
+        sb.append(i).append('\n');
+        arr[i]--;
+      }
+    }
+    System.out.println(sb);
+  }
+}
---
+++ src/정렬/_2750_1.java
@@ -0,0 +1,28 @@
+package Á¤·Ä;
+
+import java.io.*;
+import java.util.*;
+
+class _2750_1 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+
+    int[] arr = new int[n];
+
+    for(int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    Arrays.sort(arr);
+
+    for(int val : arr) {
+      sb.append(val).append('\n');
+    }
+
+    System.out.println(sb);
+  }
+}
