fc5e12bfc24dc1379808c2fe007448a0f300defb
authorsyok <authorsyok@gmail.com>
2022-07-16 10:49:48 +0900

    .

--- src/그리디알고리즘/Main.java
+++ src/그리디알고리즘/Main.java
@@ -11,26 +11,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str;
 		
-		int[] arr = new int[9];
-		int max = 0;
-		int count = 0;
-		
-		for(int i = 0; i < 9; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		while((str = br.readLine()) != null) {
+			System.out.println(str);
 		}
-		
-		for(int i = 0; i < 9; i++) {
-			if(arr[i] > max) {
-				max = arr[i];
-				count = i;
-			}
-		}
-		
-		System.out.println(max);
-		System.out.println(count + 1);
 	}
 }
+
 
 
 
