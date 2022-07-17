6c7206db30b2ca955b3730d32bddd9dfe9f18464
authorsyok <authorsyok@gmail.com>
2022-07-17 23:19:48 +0900

    .

--- src/그리디알고리즘/Main.java
+++ src/그리디알고리즘/Main.java
@@ -1,22 +1,51 @@
 package 그리디알고리즘;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String str;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		while((str = br.readLine()) != null) {
-			System.out.println(str);
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+	
+		int[] arr = new int[n];
+	
+		long max = 0;
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(max < arr[i]) {
+				max = arr[i];
+			}
 		}
+		
+		max++;	//upperbound 땜시 +1 해줌
+		
+		long min = 0;
+		long mid = 0;
+		
+		while(min < max) {
+			mid = (min + max) / 2;
+			
+			long count = 0;
+			
+			for(int i = 0; i < arr.length; i++) {
+				count += (arr[i] / mid);
+			}
+			
+			if(count < n) {
+				max = mid;
+			}
+			else {
+				min = mid + 1;
+			}
+		}
+		
+		System.out.println(min - 1);
+		
 	}
 }
 
